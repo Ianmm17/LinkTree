f5e93af44959433c654b30cb7db763edea3858ad
ianmm17 <ianmm17@yahoo.com>
2023-11-06 16:24:55 -0600

    wip basic foundation of linktree

---
+++ link_tree/src/App.js
@@ -0,0 +1,49 @@
+import logo from './wwt.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+          MONEY
+        </p>
+        <div className="links">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GICWWT.com
+        </a>
+        </div>
+        <div className="links">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GICWWT.com
+        </a>
+        </div>
+        <div className="links">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GICWWT.com
+        </a>
+        </div>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
